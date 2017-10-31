395c9e12692480d0758f46e4d6810341c6708162
SeanNaren <seannaren@hotmail.com>
2017-10-31 15:51:54 +0000

    Update script to expose layer, add volatile on var

--- benchmark_fp16_single.py
+++ benchmark_fp16_single.py
@@ -1,11 +1,7 @@
-# This script based on the parameters will benchmark the speed of F16 precision to F32 precision on GPU for 1 LSTM
 import argparse
 import time
 import torch
 from torch.autograd import Variable
-from warpctc_pytorch import CTCLoss
-
-from model import DeepSpeech
 from data.utils import update_progress
 
 parser = argparse.ArgumentParser()
@@ -14,28 +10,28 @@ parser.add_argument('--seconds', type=int, default=15)
 parser.add_argument('--dry_runs', type=int, default=20)
 parser.add_argument('--runs', type=int, default=50)
 parser.add_argument('--hidden_size', default=640, type=int)
-
+parser.add_argument('--num_layers', default=4, type=int)
 args = parser.parse_args()
 hidden_size = args.hidden_size
 input_standard = torch.randn(750, args.batch_size, hidden_size).cuda()  # seq_length based on max deepspeech length
 
 model = torch.nn.Sequential(
-    torch.nn.LSTM(hidden_size, hidden_size)
+    torch.nn.LSTM(hidden_size, hidden_size, num_layers=args.num_layers)
 )
 model = model.cuda()
 model.eval()
 
 
 def run_benchmark(input_data):
-    input_data = Variable(input_data)
+    input_data = Variable(input_data, volatile=True)
     for n in range(args.dry_runs):
-        input_data = Variable(input_data.data)
+        input_data = Variable(input_data.data, volatile=True)
         model(input_data)
         update_progress(n / (float(args.dry_runs) - 1))
     print('\nDry runs finished, running benchmark')
     running_time = 0
     for n in range(args.runs):
-        input_data = Variable(input_data.data)
+        input_data = Variable(input_data.data, volatile=True)
         start = time.time()
         model(input_data)
         end = time.time()
@@ -52,6 +48,5 @@ print("\nRunning half precision benchmark")
 run_time_half = run_benchmark(input_half)
 
 print('\n')
-print("Average times for Forward pass for LSTM: ")
 print("F32 precision: Forward time %.2fs " % run_time)
 print("F16 precision: Forward time %.2fs " % run_time_half)
